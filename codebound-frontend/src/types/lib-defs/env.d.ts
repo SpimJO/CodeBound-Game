d1adcb645345617b4030bf2d3625f8f079485c0c
SpimJO <jerichocanlas65@gmail.com>
2026-04-02 17:55:46 +0800

    feat: migrate database from MySQL to PostgreSQL and update environment configurations

--- codebound-frontend/src/types/lib-defs/env.d.ts
+++ codebound-frontend/src/types/lib-defs/env.d.ts
@@ -1,6 +1,8 @@
 interface ImportMetaEnv {
     readonly VITE_BACKEND_WS_BASE_URL: string;
     readonly VITE_BACKEND_BASE_URL: string;
+    readonly VITE_SUPABASE_URL: string;
+    readonly VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY: string;
 
     readonly PROD_SERVER_HOST: string;
     readonly PROD_SERVER_ALLOWED_HOST: string;
